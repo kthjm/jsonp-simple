de5d25a7b060edd8cdb744269aea967779abedd5
kthjm <toxictoxer@gmail.com>
2017-11-21 10:32:59 +0900

    Modify package.json

--- .rollup/rollup.config.umd.js
+++ .rollup/rollup.config.umd.js
@@ -5,7 +5,7 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   input: './src/index.js',
-  plugins: [flow({ pretty: true }), uglify({}, minify), babel()],
+  plugins: [flow({ pretty: true }), babel(), uglify({}, minify)],
   output: {
     format: 'umd',
     file: './dist/jsonp-simple.min.js',
